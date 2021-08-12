0fe56b279cfac5f6ab9d81cd570254e6c8d47529
LashaOn <83548643+LashaOn@users.noreply.github.com>
2021-08-12 06:11:27 +0430

    front project 'digikala main page ui' without responsive

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="time"
+    document.getElementById("time").innerHTML = hours + " : "
+        + minutes + " : " + seconds ;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "EXPIRED";
+    }
+}, 1000);
